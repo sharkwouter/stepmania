ee6db7e1db01ba31132c3446fb046753b04b48b4
Wouter Wijsman <wwijsman@live.nl>
2025-09-01 22:48:31 +0200

    Upgrade zlib and use submodule

---
+++ extern/zlib/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/wouter/Sources/stepmania/extern/zlib
+# Build directory: /home/wouter/Sources/stepmania/extern/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "/home/wouter/Sources/stepmania/extern/zlib/CMakeLists.txt;203;add_test;/home/wouter/Sources/stepmania/extern/zlib/CMakeLists.txt;0;")
+add_test(example64 "example64")
+set_tests_properties(example64 PROPERTIES  _BACKTRACE_TRIPLES "/home/wouter/Sources/stepmania/extern/zlib/CMakeLists.txt;212;add_test;/home/wouter/Sources/stepmania/extern/zlib/CMakeLists.txt;0;")
