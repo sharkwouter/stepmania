9eb1e7bd83cadbbd357d85b735b54ab78749419b
Wouter Wijsman <wwijsman@live.nl>
2025-09-03 00:07:59 +0200

    Testdsadas

--- extern/CMakeProject-png.cmake
+++ extern/CMakeProject-png.cmake
@@ -1,7 +1,21 @@
-if(WITH_SYSTEM_PNG)
+if(WITH_SYSTEM_PNG OR WITH_SYSTEM_ZLIB)
+  find_package(ZLIB REQUIRED)
   find_package(PNG REQUIRED)
   set(PNG_LIBRARIES ${PNG_LIBRARIES} PARENT_SCOPE)
 else()
+  set(ZLIB_BUILD_EXAMPLES OFF)
+  set(ZLIB_BUILD_SHARED OFF)
+  set(SKIP_INSTALL_ALL ON)
+  # set(BUILD_SHARED_LIBS OFF)
+  include(FetchContent)
+  FetchContent_Declare(ZLIB
+    GIT_REPOSITORY https://github.com/madler/zlib.git
+    GIT_TAG v1.3.1
+    GIT_SHALLOW TRUE
+    # PATCH_COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/zlib.patch
+    # UPDATE_DISCONNECTED 1
+    FIND_PACKAGE_ARGS NAMES ZLIB::ZLIB
+  )
   set(PNG_TOOLS OFF)
   set(PNG_SHARED OFF)
   set(PNG_FRAMEWORK OFF)
@@ -15,5 +29,5 @@ else()
     # PATCH_COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/png.patch
     # UPDATE_DISCONNECTED 1
   )
-  FetchContent_MakeAvailable(PNG)
+  FetchContent_MakeAvailable(ZLIB PNG)
 endif()
--- extern/CMakeProject-zlib.cmake
+++ extern/CMakeProject-zlib.cmake
@@ -1,18 +1,1 @@
-if(WITH_SYSTEM_ZLIB)
-  find_package(ZLIB REQUIRED)
-else()
-  set(ZLIB_BUILD_EXAMPLES OFF)
-  set(ZLIB_BUILD_SHARED OFF)
-  set(SKIP_INSTALL_ALL ON)
-  # set(BUILD_SHARED_LIBS OFF)
-  include(FetchContent)
-  FetchContent_Declare(ZLIB
-    GIT_REPOSITORY https://github.com/madler/zlib.git
-    GIT_TAG v1.3.1
-    GIT_SHALLOW TRUE
-    # PATCH_COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/zlib.patch
-    # UPDATE_DISCONNECTED 1
-    FIND_PACKAGE_ARGS NAMES ZLIB::ZLIB zlibstatic
-  )
-  FetchContent_MakeAvailable(ZLIB)
-endif()
+
