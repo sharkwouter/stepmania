8e06503fb6ca6ac5037b8639d27dde6363dd2e3b
Wouter Wijsman <wwijsman@live.nl>
2025-09-02 23:40:40 +0200

    Test without patches

--- extern/CMakeProject-zlib.cmake
+++ extern/CMakeProject-zlib.cmake
@@ -2,13 +2,13 @@ if(NOT WITH_SYSTEM_ZLIB)
   set(ZLIB_BUILD_EXAMPLES OFF)
   set(ZLIB_BUILD_SHARED OFF)
   set(SKIP_INSTALL_ALL ON)
-  set(BUILD_SHARED_LIBS OFF)
+  # set(BUILD_SHARED_LIBS OFF)
   include(FetchContent)
   FetchContent_Declare(ZLIB
     GIT_REPOSITORY https://github.com/madler/zlib.git
     GIT_TAG v1.3.1
     GIT_SHALLOW TRUE
-    PATCH_COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/zlib.patch
+    # PATCH_COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/zlib.patch
     UPDATE_DISCONNECTED 1
     OVERRIDE_FIND_PACKAGE
   )
