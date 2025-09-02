bfc26ee8b4bb63b34e42ce248320b4d862cfda7b
Wouter Wijsman <wwijsman@live.nl>
2025-09-02 20:58:41 +0200

    Add patch to prevent libpng build from failing if zlib is not installed

--- extern/CMakeProject-png.cmake
+++ extern/CMakeProject-png.cmake
@@ -6,11 +6,14 @@ else()
   set(PNG_SHARED OFF)
   set(PNG_FRAMEWORK OFF)
   set(PNG_TESTS OFF)
+  set(SKIP_INSTALL_ALL ON)
   include(FetchContent)
   FetchContent_Declare(PNG
     GIT_REPOSITORY https://github.com/pnggroup/libpng.git
     GIT_TAG v1.6.50
     GIT_SHALLOW TRUE
+    PATCH_COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/png.patch
+    UPDATE_DISCONNECTED 1
   )
   FetchContent_MakeAvailable(PNG)
 endif()
